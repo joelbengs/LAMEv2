1ae8af4e49d41c9c464947a9531786ebe50f9a09
Joel Bengs <joel.bengs@gmail.com>
2022-03-03 12:02:04 +0100

    work on ngrx

--- ng-project/src/app/my-counter/my-counter.component.ts
+++ ng-project/src/app/my-counter/my-counter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { increment, decrement, reset } from '../counter.actions';
 
 @Component({
   selector: 'app-my-counter',
@@ -7,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyCounterComponent implements OnInit {
 
-  constructor() { }
+  count$: Observable<number>;
+ 
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+ 
+  increment() {
+    this.store.dispatch(increment());
+  }
+ 
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+ 
+  reset() {
+    this.store.dispatch(reset());
+  }
 
   ngOnInit(): void {
   }
