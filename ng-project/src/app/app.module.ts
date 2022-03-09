21ea47cc3a159d8b761b8f7873a97ec9fd44cc0a
Joel Bengs <joel.bengs@gmail.com>
2022-03-09 18:42:46 +0100

    Started with material

--- ng-project/src/app/app.module.ts
+++ ng-project/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AlbumsComponent } from './albums/albums.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
 
 import { MyCounterComponent } from './my-counter/my-counter.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
@@ -32,6 +36,8 @@ import { ViewPlaylistComponent } from './view-playlist/view-playlist.component';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
     StoreModule.forRoot({ playlist: songReducer })
   ],
   providers: [],
