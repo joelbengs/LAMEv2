64227604ae43a167df696d563676901141ab8b9f
Joel Bengs <joel.bengs@gmail.com>
2022-03-09 11:09:05 +0100

    work on view-album, to display data

--- ng-project/src/app/view-album/view-album.component.ts
+++ ng-project/src/app/view-album/view-album.component.ts
@@ -12,8 +12,16 @@ export class ViewAlbumComponent implements OnInit {
   clientId: String = '9d4736d22394473c92fa72cebcd4f9d8';
   clientSecret: String = '0262845f4af74aaab6caebd7e4f03f84';
   albumID: String = "28enuddLPEA914scE6Drvk";
+ 
+  artist: String;
+  albumName: String;
+  releaseDate: String;
 
-  constructor(private service: AlbumService) { }
+  constructor(private service: AlbumService) { 
+    this.artist="";
+    this.albumName="";
+    this.releaseDate="";
+  }
 
   ngOnInit(): void {
   }
@@ -21,6 +29,10 @@ export class ViewAlbumComponent implements OnInit {
   async getAlbum() {
     let token = await this.service.getToken(this.clientId, this.clientSecret);
     let album = await this.service.getAlbum(token, this.albumID);
+    this.artist = album.artist;
+    console.log
+    this.albumName = album.name;
+    this.releaseDate = album.release_date;
     console.dir(album);
   }
 
