f6d374ea8597f295aadc4ceee4eba732cb2d5229
Joel Bengs <joel.bengs@gmail.com>
2022-03-10 18:53:41 +0100

    deleted unused component

--- ng-project/src/app/app.module.ts
+++ ng-project/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AlbumsComponent } from './albums/albums.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
@@ -26,7 +25,6 @@ import { HomepageComponent } from './homepage/homepage.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AlbumsComponent,
     SearchBarComponent,
     DiscographyComponent,
     ViewAlbumComponent,
