ab1b82f825dfa54da8f5eaa9798b9d9240bede8f
Henrik Shadman <henrik@Henriks-MacBook-Pro.local>
2022-03-10 09:30:03 +0100

    router + service

---
+++ ng-project/src/app/services/discography-service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class DiscographyService {
+        constructor() {
+
+        }
+
+        getToken = async (clientId: String, clientSecret: String) => {
+            let result = await fetch('https://accounts.spotify.com/api/token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/x-www-form-urlencoded',
+                    'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+                },
+                body: 'grant_type=client_credentials'
+            });
+
+            let data = await result.json();
+            return data.access_token;
+        }
+
+        getDiscography = async (token: any, artistID: any) =>{
+          let result = await fetch("https://api.spotify.com/v1/artists/" + artistID + "/albums?limit=5", {
+            method: 'GET',
+            headers: {
+              'Content-Type' : 'application/json',
+              'Authorization':'Bearer ' + token
+            }
+          });
+          const data = await result.json();
+          return data;
+        }
+
+    }
