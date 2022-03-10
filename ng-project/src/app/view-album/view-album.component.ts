fad82e01025f8f4a42d75d1ce93b85ffe79f56eb
Joel Bengs <joel.bengs@gmail.com>
2022-03-10 13:37:59 +0100

    style work

--- ng-project/src/app/view-album/view-album.component.ts
+++ ng-project/src/app/view-album/view-album.component.ts
@@ -78,6 +78,7 @@ export class ViewAlbumComponent implements OnInit {
     console.dir(album);
   }
 
+  //To be removed or worked on. ALSO REMOVE THE BUTTON IN HTML
   added(song: Song) {
  /*    const index = this.playlist$.map(e => e.track).indexOf(song.track, 0);
     if (index > -1) {
